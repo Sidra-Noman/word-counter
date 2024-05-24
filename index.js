b98e7392da77a2bce080b9966af5e57406d28754
Sidra Noman <sidranoman09@gmail.com>
2024-05-24 16:42:33 +0500

    push my word counter to github

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Declare a constant 'answer' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter Your Sentence to count the words:"
+    }
+]);
+//  to remove white spaces 
+const words = answers.Sentence.trim().split(" ");
+//  Print the array of words to the console
+console.log(words);
+// Print the word count of the sentence to the console
+console.log(`Your word count is ${words.length}`);
